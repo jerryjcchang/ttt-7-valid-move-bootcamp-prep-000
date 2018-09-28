7106dea5eba422d9994483c94322c84db494ec83
JC Chang <jerryjcchang@gmail.com>
2018-09-28 23:28:55 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -15,7 +15,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if position_taken?(board, index)  == false
+  if position_taken?(board, index)  == false && index.between?(0, 8) == false
     return true
   else index.between?(0, 8) == false
     return nil
